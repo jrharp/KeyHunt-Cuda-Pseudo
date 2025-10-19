ed8d2226184e893b555db24d63af1e086c68cda8
jrharp <79423117+jrharp@users.noreply.github.com>
2025-10-20 08:18:37 +1000

    Restore compatibility of GPU Base58 helper with CUDA 13 toolchain

--- KeyHunt-Cuda/GPU/GPUBase58.h
+++ KeyHunt-Cuda/GPU/GPUBase58.h
@@ -15,24 +15,29 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <stdint.h>
+
 // ---------------------------------------------------------------------------------
 // Base58
 // ---------------------------------------------------------------------------------
 
-__device__ __constant__ char *pszBase58 = (char *)"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+__device__ __constant__ char pszBase58[58] = {
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+    'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-__device__ __constant__ int8_t b58digits_map[] = {
+__device__ __constant__ int8_t b58digits_map[128] = {
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-            -1, 0, 1, 2, 3, 4, 5, 6,  7, 8, -1, -1, -1, -1, -1, -1,
-            -1, 9, 10, 11, 12, 13, 14, 15, 16, -1, 17, 18, 19, 20, 21, -1,
-            22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, -1, -1, -1, -1, -1,
-            -1, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, -1, 44, 45, 46,
-            47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, -1, -1, -1, -1,
-        };
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8, -1, -1, -1, -1, -1, -1,
+    -1,  9, 10, 11, 12, 13, 14, 15, 16, -1, 17, 18, 19, 20, 21, -1,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, -1, -1, -1, -1, -1,
+    -1, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, -1, 44, 45, 46,
+    47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, -1, -1, -1, -1};
 
-__device__ __noinline__ void _GetAddress(int type, uint32_t *hash, char *b58Add)
+__device__ __noinline__ void _GetAddress(uint32_t *__restrict__ hash, char *__restrict__ b58Add)
 {
 
     uint32_t addBytes[16];
@@ -43,26 +48,21 @@ __device__ __noinline__ void _GetAddress(int type, uint32_t *hash, char *b58Add)
     unsigned char digits[128];
 
     A[0] = 0x00;
-    memcpy(A + 1, (char *)hash, 20);
+    const unsigned char *hashBytes = reinterpret_cast<const unsigned char *>(hash);
+    for (int i = 0; i < 20; i++)
+        A[1 + i] = hashBytes[i];
 
     // Compute checksum
 
-    addBytes[0] = __byte_perm(hash[0], (uint32_t)A[0], 0x4012);
+    addBytes[0] = __byte_perm(hash[0], static_cast<uint32_t>(A[0]), 0x4012);
     addBytes[1] = __byte_perm(hash[0], hash[1], 0x3456);
     addBytes[2] = __byte_perm(hash[1], hash[2], 0x3456);
     addBytes[3] = __byte_perm(hash[2], hash[3], 0x3456);
     addBytes[4] = __byte_perm(hash[3], hash[4], 0x3456);
-    addBytes[5] = __byte_perm(hash[4], 0x80, 0x3456);
-    addBytes[6] = 0;
-    addBytes[7] = 0;
-    addBytes[8] = 0;
-    addBytes[9] = 0;
-    addBytes[10] = 0;
-    addBytes[11] = 0;
-    addBytes[12] = 0;
-    addBytes[13] = 0;
-    addBytes[14] = 0;
-    addBytes[15] = 0xA8;
+    addBytes[5] = __byte_perm(hash[4], 0x80u, 0x3456);
+    for (int i = 6; i < 15; i++)
+        addBytes[i] = 0u;
+    addBytes[15] = 0xA8u;
 
     SHA256Initialize(s);
     SHA256Transform(s, addBytes);
@@ -72,21 +72,18 @@ __device__ __noinline__ void _GetAddress(int type, uint32_t *hash, char *b58Add)
         addBytes[i] = s[i];
 
     addBytes[8] = 0x80000000;
-    addBytes[9] = 0;
-    addBytes[10] = 0;
-    addBytes[11] = 0;
-    addBytes[12] = 0;
-    addBytes[13] = 0;
-    addBytes[14] = 0;
-    addBytes[15] = 0x100;
+    for (int i = 9; i < 15; i++)
+        addBytes[i] = 0u;
+    addBytes[15] = 0x100u;
 
     SHA256Initialize(s);
     SHA256Transform(s, addBytes);
 
-    A[21] = ((uint8_t *)s)[3];
-    A[22] = ((uint8_t *)s)[2];
-    A[23] = ((uint8_t *)s)[1];
-    A[24] = ((uint8_t *)s)[0];
+    const unsigned char *stateBytes = reinterpret_cast<const unsigned char *>(s);
+    A[21] = stateBytes[3];
+    A[22] = stateBytes[2];
+    A[23] = stateBytes[1];
+    A[24] = stateBytes[0];
 
     // Base58
 
